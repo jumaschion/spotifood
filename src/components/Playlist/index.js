d4f7aece09179a47e00b1061c621d5b371a24fe1
Julia  Martins  Maschion <julia.maschion@ifood.com.br>
2019-12-18 00:02:47 -0300

    calling API and adjusting href images

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { PlaylistStyle } from './ButtonStyle';
+import { PlaylistStyle } from "./PlaylistStyle";
 
 class Playlist extends Component {
   render() {
-    let playlist = this.props.playlist
+    let playlist = this.props.playlist;
     return (
       <PlaylistStyle>
         <h3>{playlist.name}</h3>
+        <a
+          href="https://open.spotify.com/user/spotify"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={playlist.imageUrl} alt="" style={{ width: "60px" }} />
+        </a>
         <ul>
-          {playlist.songs.map(song => 
+          {playlist.songs.map(song => (
             <li>{song.name}</li>
-          )}
+          ))}
         </ul>
       </PlaylistStyle>
     );
   }
 }
-export default  Playlist
+export default Playlist;
--- src/service/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.get('https://api.spotify.com/v1', {})
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-            console.log(error)
-        })
